0d8de94ef3800fcc1a7ab08cae685fde26609053
TUSHAR SHINDE <140314810+Tushar-Shinde31@users.noreply.github.com>
2025-07-07 20:09:17 +0530

    Create DetectaCycleIDirectedGraph(BFS).cpp

---
+++ Graph/DetectaCycleIDirectedGraph(BFS).cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isCyclic(int N, vector<int> adj[]) {
+        vector<int> indegree(N, 0);
+
+        // Calculate indegree of each node
+        for (int i = 0; i < N; i++) {
+            for (auto it : adj[i]) {
+                indegree[it]++;
+            }
+        }
+
+        queue<int> q;
+        for (int i = 0; i < N; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+
+        int cnt = 0;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            cnt++;
+
+            for (auto it : adj[node]) {
+                indegree[it]--;
+                if (indegree[it] == 0)
+                    q.push(it);
+            }
+        }
+
+        // If all nodes are not visited, graph has a cycle
+        return cnt != N;
+    }
+};
