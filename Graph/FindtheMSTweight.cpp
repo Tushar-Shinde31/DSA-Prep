919139d499484fff217747533d9a2d4b7c1adaae
TUSHAR SHINDE <140314810+Tushar-Shinde31@users.noreply.github.com>
2025-07-16 20:09:53 +0530

    Create FindtheMSTweight.cpp

---
+++ Graph/FindtheMSTweight.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int spanningTree(int V, vector<vector<int>> adj[]) {
+        // Min-heap: {weight, node}
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+        vector<int> vis(V, 0);  // Visited array
+        pq.push({0, 0});        // Start from node 0 with weight 0
+
+        int sum = 0;            // Total weight of MST
+
+        while (!pq.empty()) {
+            auto it = pq.top();
+            pq.pop();
+
+            int wt = it.first;
+            int node = it.second;
+
+            if (vis[node]) continue;
+
+            vis[node] = 1;
+            sum += wt;
+
+            for (auto edge : adj[node]) {
+                int adjNode = edge[0];
+                int edW = edge[1];
+
+                if (!vis[adjNode]) {
+                    pq.push({edW, adjNode});
+                }
+            }
+        }
+
+        return sum;
+    }
+};
