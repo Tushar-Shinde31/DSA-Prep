46e845fbe98903f77afd8e9a4f509c840729274a
Tushar-Shinde45 <149402930+Tushar-Shinde45@users.noreply.github.com>
2025-07-22 18:21:35 +0530

    Add implementation of 0/1 Knapsack problem using dynamic programming

--- Dynamic Prg/Knapsack.cpp
+++ Dynamic Prg/Knapsack.cpp
@@ -7,22 +7,30 @@ public:
             return 0;
         }
 
+
         if (dp[ind][capacity] != -1)
             return dp[ind][capacity];
+
 
         int include = 0;
         if (wt[ind] <= capacity) {
             include = val[ind] + solve(wt, val, ind - 1, capacity - wt[ind], dp);
         }
 
+
         int exclude = 0 + solve(wt, val, ind - 1, capacity, dp);
+
 
         dp[ind][capacity] = max(include, exclude);
         return dp[ind][capacity];
     }
+
 
     int knapsack01(vector<int>& wt, vector<int>& val, int n, int W) {
         vector<vector<int>> dp(n, vector<int>(W + 1, -1));
         return solve(wt, val, n - 1, W, dp);
     }
 };
+
+
+
