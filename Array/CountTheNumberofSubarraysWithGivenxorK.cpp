f242328a045281f156be2f41aa907a50cf31b3c9
TUSHAR SHINDE <140314810+Tushar-Shinde31@users.noreply.github.com>
2025-07-30 16:01:12 +0530

    Create CountTheNumberofSubarraysWithGivenxorK.cpp

---
+++ Array/CountTheNumberofSubarraysWithGivenxorK.cpp
@@ -0,0 +1,19 @@
+int subarraysWithXorK(vector<int> a, int k) {
+    int n = a.size(); //size of the given array.
+    int cnt = 0;
+
+    // Step 1: Generating subarrays:
+    for (int i = 0; i < n; i++) {
+        int xorr = 0;
+        for (int j = i; j < n; j++) {
+
+            //step 2:calculate XOR of all
+            // elements:
+            xorr = xorr ^ a[j];
+
+            // step 3:check XOR and count:
+            if (xorr == k) cnt++;
+        }
+    }
+    return cnt;
+}
